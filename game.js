b2df4b56b34cb106331f66ab5a694b7d9fe0acb3
tebracb <bori.temesi@gmail.com>
2019-05-24 13:01:19 +0200

    Building character class
    also added new map and illustrations

--- game.js
+++ game.js
@@ -17,13 +17,6 @@ const SQUARE = $("<div />", {
 
 
 
-
-let veggies = {
-    src: "img/char2.png"
-}
-
-let veggieSrc = veggies.src;
-
 $(document).ready(function () {
     //add columns to the the temp row object
     for (var i = 0; i < COLUMNS; i++) {
@@ -36,12 +29,12 @@ $(document).ready(function () {
     changeTilesMountains();
     changeTilesWeapons();
     addCharacters();
-  
-   
-    loopOverAdjacentTiles(character1Position);
-    loopOverAdjacentTiles(character2Position);
 
-    characterMovement(character1Position, fruitSrc);
+
+    loopOverAdjacentTiles(fruits.calculatePosition());
+    loopOverAdjacentTiles(veggies.calculatePosition());
+
+    characterMovement(fruits.calculatePosition(), fruits.src);
 
 });
 
@@ -65,7 +58,7 @@ function changeTilesMountains() {
 
         $(selectRandomTile()).attr('src', 'img/vakond2.png').addClass("occupiedTile");
         $(selectRandomTile()).attr('src', 'img/fuszal.png').addClass("occupiedTile");
-        
+
     }
 };
 
@@ -79,94 +72,100 @@ function changeTilesWeapons() {
         $(selectRandomTile()).attr('src', 'img/szocske.png').addClass("occupiedTile");
         $(selectRandomTile()).attr('src', 'img/hernyo.png').addClass("occupiedTile");
     };
-      
-    };
 
-    class Character {
-        constructor (src) {
-            this.src = src;
-           
+};
+
+class Character {
+    constructor(src, confidenceLevel) {
+        this.src = src;
+        this.cofidenceLevel = confidenceLevel;
+
+    }
+
+    calculatePosition() {
+        if (this.src === fruitSrc) {
+            return $('.fruit').index('.mainTile');
+        } else {
+            return $('.veggie').index('.mainTile');
         }
     };
-    let fruitSrc = "img/char1.png";
-    let fruits = new Character (fruitSrc);
+
+};
+
+function characterMovement(characterPosition, characterImgSrc) {
+    $.each((getAdjacentTiles(characterPosition)), function () {
+        $(this).on('click', function () {
+            $('.mainTile:eq(' + (characterPosition) + ')').attr('src', 'img/dirtMainTile.png');
+            $('.mainTile:eq(' + (characterPosition) + ')').removeClass("character fruit veggie occupiedTile");
+            $(this).attr('src', characterImgSrc);
+            $(this).addClass("character fruit occupiedTile");
+            characterPosition = $('.fruit').index('.mainTile');
+
+            loopOverAdjacentTiles(characterPosition);
+
+            console.log(characterPosition);
+        });
+
+    });
+}
 
 
-function getAdjacentTiles(indexPosition) {
+let fruitSrc = "img/char1.png";
+let fruits = new Character(fruitSrc, 100);
 
+let veggieSrc = "img/char2.png";
+let veggies = new Character(veggieSrc, 100);
 
-    // $('.mainTile:eq(' + (character1Position+10)  + ')').hide(2000);
+function addCharacters() {
+    $(selectRandomTile()).attr('src', fruitSrc).addClass("occupiedTile character fruit");
+    $(selectRandomTile()).attr('src', veggieSrc).addClass("occupiedTile character veggie");
+
+};
+
+function getAdjacentTiles(characterPosition) {
+
 
 
     let adjacentTiles = [];
 
-        if(!((indexPosition+1) % 10 === 0) && !($('.mainTile:eq(' + (indexPosition+1) + ')').hasClass("occupiedTile"))) {
-            adjacentTiles.push($('.mainTile:eq(' + (indexPosition+1) + ')'));
-        }
+    if (!((characterPosition + 1) % 10 === 0) && !($('.mainTile:eq(' + (characterPosition + 1) + ')').hasClass("occupiedTile"))) {
+        adjacentTiles.push($('.mainTile:eq(' + (characterPosition + 1) + ')'));
+    }
 
-        if(!(indexPosition % 10 === 0) && !($('.mainTile:eq(' + (indexPosition-1) + ')').hasClass("occupiedTile"))) {
-            adjacentTiles.push($('.mainTile:eq(' + (indexPosition-1) + ')'));
-        }
+    if (!(characterPosition % 10 === 0) && !($('.mainTile:eq(' + (characterPosition - 1) + ')').hasClass("occupiedTile"))) {
+        adjacentTiles.push($('.mainTile:eq(' + (characterPosition - 1) + ')'));
+    }
 
-        if(indexPosition > 9 && !($('.mainTile:eq(' + (indexPosition-10) + ')').hasClass("occupiedTile")))  {
-            adjacentTiles.push($('.mainTile:eq(' + (indexPosition-10) + ')'));
-        }
-        
-        if(indexPosition < 90 && !($('.mainTile:eq(' + (indexPosition+10) + ')').hasClass("occupiedTile")))  {
-            adjacentTiles.push($('.mainTile:eq(' + (indexPosition+10) + ')'));
-        }
-        
-    
+    if (characterPosition > 9 && !($('.mainTile:eq(' + (characterPosition - 10) + ')').hasClass("occupiedTile"))) {
+        adjacentTiles.push($('.mainTile:eq(' + (characterPosition - 10) + ')'));
+    }
+
+    if (characterPosition < 90 && !($('.mainTile:eq(' + (characterPosition + 10) + ')').hasClass("occupiedTile"))) {
+        adjacentTiles.push($('.mainTile:eq(' + (characterPosition + 10) + ')'));
+    }
+
+
     return adjacentTiles;
 };
 
 
 function loopOverAdjacentTiles(characterPosition) {
-$.each((getAdjacentTiles(characterPosition)), function() {
-    this.css("border", "1px solid #ed2f30");
-    this.css("box-sizing", "border-box");
+    $.each((getAdjacentTiles(characterPosition)), function () {
+        this.css("border", "1px solid #ed2f30");
+        this.css("box-sizing", "border-box");
     });
 };
 
 
 
 
-let character1Position;
-let character2Position;
-
-function addCharacters() {
-    $(selectRandomTile()).attr('src', 'img/char1.png').addClass("occupiedTile character character1");
-    $(selectRandomTile()).attr('src', 'img/char2.png').addClass("occupiedTile character character2");
-
-    character1Position = $('.character1').index('.mainTile');
-    character2Position = $('.character2').index('.mainTile');
-   
-};
 
 // function endRound() {
-//     getAdjacentTiles(character2Position);
-//     loopOverAdjacentTiles(character2Position);
+//     getAdjacentTiles(veggiePosition);
+//     loopOverAdjacentTiles(veggiePosition);
 // }
 
 // function togglePlayer(){
-    
+
 // }
 
-function characterMovement(characterPosition, characterImgSrc) {
-    $.each((getAdjacentTiles(characterPosition)), function() {
-        $(this).on('click', function () {
-            $('.mainTile:eq(' + (characterPosition) + ')').attr('src', 'img/dirtMainTile.png');
-            $('.mainTile:eq(' + (characterPosition) + ')').removeClass("character character1 character2 occupiedTile");
-            $(this).attr('src', characterImgSrc);
-            $(this).addClass("character character1 occupiedTile");
-            characterPosition = $('.character1').index('.mainTile');
-
-            loopOverAdjacentTiles(characterPosition);
-            
-           console.log(characterPosition);
-        });
-        
-        });
-        
-
-}
